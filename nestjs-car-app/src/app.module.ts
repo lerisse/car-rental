d3e34b6bac2a85e9abc09366089099a78216954e
lerisse <tristanlerisse@gmail.com>
2021-10-22 21:01:19 -0400

    EOD

--- nestjs-car-app/src/app.module.ts
+++ nestjs-car-app/src/app.module.ts
@@ -3,9 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule],
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    GraphQLModule.forRoot({
+      playground: (process.env.NODE_ENV === 'development' && true) || false,
+      debug: (process.env.NODE_ENV === 'development' && true) || false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
